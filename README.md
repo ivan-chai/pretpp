e024b587d31dbb4443e0fd316eb282cede2e1f6b
Ivan Karpukhin <karpuhini@yandex.ru>
2025-01-09 14:12:23 +0300

    Add GPT-style training

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PreTPP
 Advanced pretraining for TPP, MTPP and Event Sequences.
+
+The code is highly dependent on and compatible with [HoTPP](https://github.com/ivan-chai/hotpp-benchmark).
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+import setuptools
+
+with open("README.md") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="pretpp",
+    version="0.0.1",
+    author="Ivan Karpukhin",
+    author_email="karpuhini@yandex.ru",
+    description="Advanced pretraining for TPP, MTPP and Event Sequences.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(include=["pretpp", "pretpp.*"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=[
+        "hotpp-benchmark>=0.1.0"
+    ]
+)
