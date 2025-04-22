fe78d1792ffc30f60512698054c3e9989af11894
Ivan Karpukhin <karpuhini@yandex.ru>
2025-04-22 17:05:52 +0300

    Fix name

--- pretpp/downstream.py
+++ pretpp/downstream.py
@@ -346,7 +346,7 @@ class DownstreamCheckpointCallback(pl.callbacks.Checkpoint):
         if not os.path.isdir(self._root):
             os.mkdir(self._root)
         checkpoint = pl_module.state_dict()
-        last_checkpoint_path = os.path.join(self._root, "last.pth")
+        last_checkpoint_path = os.path.join(self._root, "checkpoint-last.pth")
         torch.save(checkpoint, last_checkpoint_path)
         self._fetch_metrics(trainer, pl_module)
 
