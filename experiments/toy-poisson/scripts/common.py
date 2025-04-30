8c3b1a40956b88c25490294ecfa4d8953c3638b2
Ivan Karpukhin <karpuhini@yandex.ru>
2025-04-30 11:46:41 +0300

    Add poisson, more configs

---
+++ experiments/toy-poisson/scripts/common.py
@@ -0,0 +1,53 @@
+import math
+import numpy as np
+
+
+class Preset:
+    def __init__(self, args):
+        self.rates = np.random.rand(args.n_labels) * args.max_frequency
+        self.length = args.length
+
+    def generate(self):
+        # 1. Generate L events for each label.
+        labels = []
+        timestamps = []
+        for label, rate in enumerate(self.rates):
+            scale = 1 / rate
+            timestamps.append(np.cumsum(np.random.exponential(scale, size=self.length)))
+            labels.append(np.full([self.length], label))
+        # 2. Merge sequences and truncate to the specified length.
+        timestamps = np.concatenate(timestamps)
+        labels = np.concatenate(labels)
+        order = np.argsort(timestamps)
+        timestamps = timestamps[order][:self.length].tolist()
+        labels = labels[order][:self.length].tolist()
+        return timestamps, labels
+
+    def log_like(self, timestamps, labels):
+        # Label streams are independent. Log-likelihood is a sum of log-likelihoods for each label.
+        timestamps = np.asarray(timestamps)
+        labels = np.asarray(labels)
+        l = 0
+        for label, rate in enumerate(self.rates):
+            mask = labels == label
+            if mask.sum() == 0:
+                continue
+            lts = timestamps[mask]
+            extended = np.concatenate([np.zeros_like(lts[:1]), lts])
+            intertimes = extended[1:] - extended[:-1]
+            log_pdfs = math.log(rate) - rate * intertimes
+            l += log_pdfs.sum()
+        return l
+
+
+class Model:
+    def __init__(self, args):
+        self.presets = [Preset(args) for _ in range(args.n_presets)]
+
+    def generate(self):
+        target = int(np.random.choice(len(self.presets)))
+        timestamps, labels = self.presets[target].generate()
+        return timestamps, labels, target
+
+    def log_likes(self, timestamps, labels):
+        return [p.log_like(timestamps, labels) for p in self.presets]
