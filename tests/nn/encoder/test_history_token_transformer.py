c3525e1be73e635cb6c1ab8abc1f2d89f3137789
Ivan Karpukhin <karpuhini@yandex.ru>
2025-05-28 16:37:58 +0300

    Uniform HT transformer

--- tests/nn/encoder/test_history_token_transformer.py
+++ tests/nn/encoder/test_history_token_transformer.py
@@ -5,11 +5,11 @@ from unittest import TestCase, main
 import torch
 
 from hotpp.data import PaddedBatch
-from pretpp.nn.encoder.history_token_transformer import HistoryTokenTransformer, insert_tokens, sample_mask
+from pretpp.nn.encoder.history_token_transformer import HistoryTokenTransformer, insert_tokens, remove_tokens, make_mask, sample_mask
 
 
 class TestHistoryTokenTransformer(TestCase):
-    def test_insert_tokens(self):
+    def test_insert_remove_tokens(self):
         embeddings = torch.tensor([
             [0, 1, 2, 3],
             [4, 5, 6, 7],
@@ -20,48 +20,59 @@ class TestHistoryTokenTransformer(TestCase):
             [5, 6.5, 7, -1],
             [5, 5, 5, 5]
         ])
-        positions = torch.tensor([
-            [1, 3],
-            [0, 1],
-            [3, 5]  # Require sorting.
-        ])
         token = torch.tensor([-1])
-        new_embeddings, new_timestamps, source_indices = insert_tokens(embeddings, timestamps, positions, token)
+        new_embeddings, new_timestamps = insert_tokens(embeddings, timestamps, token)
 
         gt_embeddings = torch.tensor([
-            [0, -1, 1, -1, 2, 3],
-            [-1, -1, 4, 5, 6, 7],
-            [8, 9, 10, -1, 11, -1]
-        ]).reshape(3, 6, 1)
+            [0, -1, 1, -1, 2, -1, 3, -1],
+            [4, -1, 5, -1, 6, -1, 7, -1],
+            [8, -1, 9, -1, 10, -1, 11, -1]
+        ]).reshape(3, 8, 1)
         self.assertTrue((new_embeddings == gt_embeddings).all())
 
         gt_timestamps = torch.tensor([
-            [0, 0, 1.5, 1.5, 3, 4],
-            [5, 5, 5, 6.5, 7, -1],
-            [5, 5, 5, 5, 5, 5]
+            [0, 0, 1.5, 1.5, 3, 3, 4, 4],
+            [5, 5, 6.5, 6.5, 7, 7, -1, -1],
+            [5, 5, 5, 5, 5, 5, 5, 5]
         ])
         self.assertTrue((new_timestamps - gt_timestamps).abs().max() < 1e-6)
 
-        gt_indices = torch.tensor([
-            [0, 2, 4, 5],
-            [2, 3, 4, 5],
-            [0, 1, 2, 4]
+        reverted_embeddings = remove_tokens(new_embeddings)
+        self.assertTrue((reverted_embeddings == embeddings).all())
+
+    def test_make_mask(self):
+        n_summarize = torch.tensor([
+            0, 2, 1
         ])
-        self.assertTrue((source_indices == gt_indices).all())
+        mask = make_mask(n_summarize)
+
+        mask_gt = torch.tensor([
+            [0, 1, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0],  # History token.
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0],  # History token.
+            [1, 0, 0, 1, 0, 1],
+            [0, 0, 0, 0, 0, 0]   # History token.
+        ]).bool()
+        self.assertTrue((mask == mask_gt).all())
 
     def test_sample_mask(self):
-        positions = torch.tensor([1, 3])
-        l = 4
-        mask = sample_mask(positions, l)  # (6, 6).
+        mask = sample_mask(3, locality=1)
         mask_gt = torch.tensor([
-            [0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 1, 0, 1],
             [0, 0, 0, 0, 0, 0],  # History token.
-            [1, 0, 0, 0, 0, 0],
+            [1, 0, 0, 1, 0, 1],
             [0, 0, 0, 0, 0, 0],  # History token.
-            [1, 1, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0, 0],
-        ])
+            [1, 1, 1, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0]   # History token.
+        ]).bool()
         self.assertTrue((mask == mask_gt).all())
+
+        for _ in range(8):
+            mask = sample_mask(3, locality=0)
+            self.assertEqual(mask.shape, (6, 6))
+            self.assertTrue((~mask[1::2]).all())
+            self.assertTrue((~mask.diag()).all())
 
 
 if __name__ == "__main__":
