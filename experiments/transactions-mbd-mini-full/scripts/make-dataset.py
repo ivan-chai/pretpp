624faebd31a3b6a8476417e90b28121916a9fc6c
MaxPoles <maks09pp@gmail.com>
2025-12-18 14:08:55 +0300

    Changed targets in dataset

--- experiments/transactions-mbd-mini-full/scripts/make-dataset.py
+++ experiments/transactions-mbd-mini-full/scripts/make-dataset.py
@@ -22,13 +22,31 @@ def parse_args():
 def make_from_folds(folds, data_root, targets_root, dst, n_partitions):
     spark = SparkSession.builder.getOrCreate()
     dataset = None
+    spark = SparkSession.builder.getOrCreate()
+    dataset = None
     for fold in folds:
         part = spark.read.parquet(os.path.join(data_root, f"fold={fold}"))
         targets = spark.read.parquet(os.path.join(targets_root, f"fold={fold}"))
-        targets = targets.drop("is_balanced") #Убираем повторяющийся столбец
+        targets = targets.drop("is_balanced", "trans_count", "dif_trans_data")
+        mon_values = targets.select("mon").distinct().rdd.flatMap(lambda x: x).collect()
+        transformed_tables = []
+        base_df = targets.select("client_id").distinct()
+        for mon_val in mon_values:
+            df_mon = targets.filter(F.col("mon") == mon_val)
+            df_mon_renamed = df_mon.select(
+                "client_id",
+                F.col("target_1").alias(f"target_1_{mon_val}"),
+                F.col("target_2").alias(f"target_2_{mon_val}"),
+                F.col("target_3").alias(f"target_3_{mon_val}"),
+                F.col("target_4").alias(f"target_4_{mon_val}"),
+            )
+            transformed_tables.append(df_mon_renamed)
+        result_df = base_df
+        for table in transformed_tables:
+            result_df = result_df.join(table, on="client_id", how="inner")
+        targets = result_df
         part = part.join(targets, on="client_id", how="inner")
         dataset = dataset.union(part) if dataset is not None else part
-    # Compute log_amount.
     udf = F.udf(lambda x: [math.log(abs(v) + 1) for v in x], ArrayType(FloatType(), False))
     dataset = dataset.withColumn("log_amount", udf(F.col("amount")))
     # Scale event_time by 4 days.
